01d11f968a9362dd4be65a910f5ce342b437892b
Manuel Schröder <manuel.schroeder@storyblok.com>
2024-09-03 10:55:15 +0200

    improvements

--- composables/getSiteConfig.js
+++ composables/getSiteConfig.js
@@ -1,31 +1,31 @@
 export default async function () {
-  const route = useRoute()
+  const folderPathTest = await getFolderPath()
+  const releaseId = await getReleaseId()
   const siteConfig = ref()
   const storyblokApi = useStoryblokApi()
   const nestedSiteConfig = ref(false)
   const apiParams = {
     version: getVersion(),
     resolve_links: 'url',
+    from_release: releaseId,
   }
 
-  if (route.query.path) {
-    const found = route.query.path.match('^(.*[\\/])')
-    if (found) {
-      const folderPath = found[0]
-      try {
-        const { data } = await storyblokApi.get(
-          `cdn/stories/${folderPath}site-config`,
-          apiParams,
-        )
-        siteConfig.value = data.story
-        nestedSiteConfig.value = true
-      } catch (error) {
-        nestedSiteConfig.value = false
-      }
+  if (folderPathTest) {
+    try {
+      // Check whether there is a site-config in the current folder
+      const { data } = await storyblokApi.get(
+        `cdn/stories/${folderPathTest}/site-config`,
+        apiParams,
+      )
+      siteConfig.value = data.story
+      nestedSiteConfig.value = true
+    } catch (error) {
+      nestedSiteConfig.value = false
     }
   }
 
   if (!nestedSiteConfig.value) {
+    // Otherwise load the default site-config
     const { data } = await storyblokApi.get(
       'cdn/stories/site-config',
       apiParams,
