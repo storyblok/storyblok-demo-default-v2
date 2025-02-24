cf1e456ddddf59380fb403e0b1e4c705f7171497
Manuel Schröder <manuel.schroeder@storyblok.com>
2025-02-24 17:12:24 +0100

    add logo section

---
+++ storyblok/LogoSection.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<template>
+  <!-- <pre>{{ blok }}</pre> -->
+  <section
+    v-editable="blok"
+    class="page-section logos-section"
+  >
+    <div class="container text-center">
+      <Lead v-if="blok.lead">
+        {{ blok.lead }}
+      </Lead>
+      <ul v-if="blok.logos.length" class="mt-16 flex items-center justify-center gap-16">
+        <li v-for="logo in blok.logos" :key="logo._uid">
+          <img v-if="logo.filename" :src="logo?.filename" :alt="logo?.alt" />
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
